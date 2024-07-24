a9451b86cf89994bbf40297ccb79ac7f596a8d23
@kube_nerd <viniciuspoa2@hotmail.com>
2024-07-24 13:27:27 -0300

    Update app.py

--- application/app.py
+++ application/app.py
@@ -119,7 +119,3 @@ class HealthCheck(Resource):
         else:
             HealthCheckModel(status="healthy")
             return "Healthy", 200
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
